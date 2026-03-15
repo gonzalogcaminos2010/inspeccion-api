fe7472358c3167038f9102ec86c1d619bbce2946
Gonzalo Caminos <gonzalogabrielcaminos@gmail.com>
2026-03-15 16:32:54 -0300

    fix: return JSON health check on root instead of Blade view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name' => 'API Inspeccion',
+        'status' => 'running',
+        'version' => 'v1',
+    ]);
 });
